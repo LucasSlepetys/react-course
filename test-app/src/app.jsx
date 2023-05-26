dbf0c3895757febccc0d4eca86026075e45f3c43
Lucas Slepetys <lucas.slepetyss@gmail.com>
2023-05-26 11:35:07 +0200

    map method

--- test-app/src/app.jsx
+++ test-app/src/app.jsx
@@ -7,12 +7,14 @@ const list_of_books = [
     img: img_1,
     title: 'Curious Mind',
     author: 'Jordan Moore',
+    key: 1,
   },
 
   {
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
     title: 'Atomic Habits',
     author: 'James Clear',
+    key: 2,
   },
 ];
 
@@ -20,7 +22,7 @@ function App() {
   return (
     <section className='booklist'>
       {list_of_books.map((book) => {
-        return <Book_template {...book} />;
+        return <Book_template {...book} key={book.key}></Book_template>;
       })}
     </section>
   );
--- test-app/src/book component/book_template.jsx
+++ test-app/src/book component/book_template.jsx
@@ -1,13 +1,11 @@
 import react from 'react';
 
-const Book_template = ({ ...props }) => {
+const Book_template = ({ img, title, author }) => {
   return (
     <div className='book'>
-      <img src={props.img} alt='This is the image of the book' />
-      <h2>{props.title}</h2>
-      <h2 style={{ color: '#DFD1D1', fontSize: '1.5rem' }}>
-        By: {props.author}
-      </h2>
+      <img src={img} alt='This is the image of the book' />
+      <h2>{title}</h2>
+      <h2 style={{ color: '#DFD1D1', fontSize: '1.5rem' }}>By: {author}</h2>
     </div>
   );
 };
