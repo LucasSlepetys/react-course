dd7e5d48d63b3d82da0f9ac37dc07d93cefc58c7
Lucas Slepetys <lucas.slepetyss@gmail.com>
2023-05-27 11:46:42 +0200

    Named export

--- test-app/src/app.jsx
+++ test-app/src/app.jsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import img_1 from './Images/book-1.jpg';
 import Book_template from './book component/book_template';
-
-const list_of_books = [
-  {
-    img: img_1,
-    title: 'Curious Mind',
-    author: 'Jordan Moore',
-    id: 1,
-  },
-
-  {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    title: 'Atomic Habits',
-    author: 'James Clear',
-    id: 2,
-  },
-];
+import { list_of_books } from './book component/bookList.js';
 
 function App() {
-  function getBook(id) {
-    const book = list_of_books.find((book) => book.id === id);
-    console.log(book);
-  }
   return (
     <section className='booklist'>
       {list_of_books.map((book) => {
-        return (
-          <Book_template
-            {...book}
-            key={book.id}
-            getBook={getBook}
-          ></Book_template>
-        );
+        return <Book_template {...book} key={book.id}></Book_template>;
       })}
     </section>
   );
--- test-app/src/book component/book_template.jsx
+++ test-app/src/book component/book_template.jsx
@@ -1,8 +1,8 @@
 import react from 'react';
 
-const Book_template = ({ img, title, author, getBook, id }) => {
+const Book_template = ({ img, title, author }) => {
   return (
-    <div className='book' onClick={() => getBook(id)}>
+    <div className='book'>
       <img src={img} alt='This is the image of the book' />
       <h2>{title}</h2>
       <h2 style={{ color: '#DFD1D1', fontSize: '1.5rem' }}>By: {author}</h2>
