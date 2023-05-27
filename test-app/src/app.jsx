4d08b2154916d781703b4dfde1c5ff2da7585f78
Lucas Slepetys <lucas.slepetyss@gmail.com>
2023-05-27 11:35:53 +0200

    Get Book function

--- test-app/src/app.jsx
+++ test-app/src/app.jsx
@@ -7,22 +7,32 @@ const list_of_books = [
     img: img_1,
     title: 'Curious Mind',
     author: 'Jordan Moore',
-    key: 1,
+    id: 1,
   },
 
   {
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
     title: 'Atomic Habits',
     author: 'James Clear',
-    key: 2,
+    id: 2,
   },
 ];
 
 function App() {
+  function getBook(id) {
+    const book = list_of_books.find((book) => book.id === id);
+    console.log(book);
+  }
   return (
     <section className='booklist'>
       {list_of_books.map((book) => {
-        return <Book_template {...book} key={book.key}></Book_template>;
+        return (
+          <Book_template
+            {...book}
+            key={book.id}
+            getBook={getBook}
+          ></Book_template>
+        );
       })}
     </section>
   );
--- test-app/src/book component/book_template.jsx
+++ test-app/src/book component/book_template.jsx
@@ -1,8 +1,8 @@
 import react from 'react';
 
-const Book_template = ({ img, title, author }) => {
+const Book_template = ({ img, title, author, getBook, id }) => {
   return (
-    <div className='book'>
+    <div className='book' onClick={() => getBook(id)}>
       <img src={img} alt='This is the image of the book' />
       <h2>{title}</h2>
       <h2 style={{ color: '#DFD1D1', fontSize: '1.5rem' }}>By: {author}</h2>
