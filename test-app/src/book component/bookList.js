91ec21435d3a09b29ad0349fc530c89d4d24f871
Lucas Slepetys <lucas.slepetyss@gmail.com>
2023-05-27 11:59:53 +0200

    All 3 books + images + fixed no more link

---
+++ test-app/src/book component/bookList.js
@@ -0,0 +1,25 @@
+import img1 from '../Images/book-1.jpg';
+import img2 from '../Images/book-2.jpg';
+import img3 from '../Images/book-3.jpg';
+
+export const books = [
+  {
+    img: img1,
+    title: 'Curious Mind',
+    author: 'Jordan Moore',
+    id: 1,
+  },
+
+  {
+    img: img2,
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    id: 2,
+  },
+  {
+    img: img3,
+    title: 'Fairy Tale',
+    author: 'Stephen King',
+    id: 3,
+  },
+];
