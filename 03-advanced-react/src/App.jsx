c2b5c87502c4f20e91fcb9e4e2797fb36848e3d7
Lucas Slepetys <lucas.slepetyss@gmail.com>
2023-06-15 01:50:35 +0200

    Finished with levareging js - starting with forms

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,9 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+import { List } from './tutorial/05-leverage-javascript/starter';
 
 function App() {
   return (
     <div className='container'>
-      <Starter></Starter>
+      <List></List>
     </div>
   );
 }
